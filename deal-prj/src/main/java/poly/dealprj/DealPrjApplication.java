76bda060ce74b3a9f695e19f17e7502e6619d822
NahHyun-Kim <cherish3_@naver.com>
2021-03-21 23:54:53 +0900

    개인프로젝트 gradle 세팅, 테스트

---
+++ deal-prj/src/main/java/poly/dealprj/DealPrjApplication.java
@@ -0,0 +1,14 @@
+package poly.dealprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealPrjApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DealPrjApplication.class, args);
+	}
+
+}
---
+++ deal-prj/src/test/java/poly/dealprj/DealPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.dealprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
